4859874af49843ece0e5ca6f23a84ffa8eefdd03
uday <udaygitlog@gmail.com>
2025-03-25 19:45:40 +0530

    feat: effects with audio

--- crop-dash/Assets/Scripts/PlayerController.cs
+++ crop-dash/Assets/Scripts/PlayerController.cs
@@ -3,26 +3,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem runParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     public float jumpForce = 15.0f;
     public float gravityModifier = 2.0f;
     public bool isGrounded = true;
-
     public bool gameOver;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+            runParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -31,10 +41,16 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            runParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle")) 
         { 
             gameOver = true;
+            runParticle.Stop();
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", Random.Range(1,3));
         }
     }
 }
